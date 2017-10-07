c5b53dd54abb0aa753ffacb1481ffefcb5445de4
gwc593 <gwc593@gmail.com>
2017-10-07 16:31:23 +0100

    Temperature Plotting

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import numpy as np
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
 if __name__ == "__main__":
+
     data_loc = 'Data/Lowestoft_Data.csv'
 
-    print(data_loc)
+    df = pd.read_csv(data_loc)
+    df['Date'] = pd.to_datetime(dict(year=df.Year, month=df.Month, day='01'))
+
+    df.reset_index()
+    df.index=df['Date']
+
+    del df['Date']
+    del df['Year']
+    del df['Month']
+
+
+    plot1 = plt.figure()
+    ax = plot1.add_subplot(1, 1, 1)
+    ax.plot(df['Tmax'], 'r-', alpha=0.5)
+    ax.plot(df['Tmin'], 'b-', alpha=0.5)
+
+
+    plt.show()
+
